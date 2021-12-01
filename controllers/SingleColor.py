aa1b1e94f83376eafa7f0e8ad2647429839b6bbd
Helge Bruegner <helge.bruegner@solytic.com>
2021-12-01 23:59:00 +0100

    add color wheel

--- controllers/SingleColor.py
+++ controllers/SingleColor.py
@@ -1,6 +1,5 @@
 from flask import request
 from flask_restful import Resource
-from werkzeug.utils import redirect
 
 from dependencies.Container import Container
 
@@ -12,14 +11,12 @@ class SingleColor(Resource):
     def post(self):
         color = request.form.get("color")
 
-        print(color)
-
         # should be valid hex: #123456
-        if len(color) == 7:
+        if color is not None and len(color) == 7:
             r = int(color[1:3], 16)
             g = int(color[3:5], 16)
             b = int(color[5:7], 16)
 
             self.star.set_color(r, g, b)
 
-        return redirect('/')
+        return {'color': color}
--- star/Star.py
+++ star/Star.py
@@ -4,7 +4,7 @@ from star.AbstractStar import AbstractStar
 class Star(AbstractStar):
     # pin 10 == GPIO 12 on the board
     def __init__(self, pin: int = 10, num_leds: int = 11):
-        # import the modules here for lazy loading
+        # lazy-load the dependencies here so that it doesn't break non-Raspberry Pi environments
         import neopixel
         from microcontroller import Pin
 
