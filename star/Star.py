f89f20eaa5f076195dfd9053164c8bff45b91f94
Helge Bruegner <helge@bruegner.de>
2021-12-02 08:28:47 +0000

    fixed pin selection

--- star/Star.py
+++ star/Star.py
@@ -2,8 +2,7 @@ from star.AbstractStar import AbstractStar
 
 
 class Star(AbstractStar):
-    # pin 10 == GPIO 12 on the board
-    def __init__(self, pin: int = 10, num_leds: int = 11):
+    def __init__(self, pin: int = 12, num_leds: int = 11):
         # lazy-load the dependencies here so that it doesn't break non-Raspberry Pi environments
         import neopixel
         from microcontroller import Pin
