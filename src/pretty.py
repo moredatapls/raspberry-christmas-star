1a3dbad63abfc51c892260c8d89117c29f7e8254
Helge Bruegner <helge@bruegner.de>
2021-12-12 21:14:28 +0000

    removed old obsolete stuff, added systemd service file

--- src/pretty.py
+++
@@ -1,6 +0,0 @@
-import board
-import neopixel
-
-pixels = neopixel.NeoPixel(board.D12, 11)
-
-pixels.fill((255,150,25))
--- src/startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo python3 /home/pi/christmas-star/src/pretty.py
